039754705abccc6903ac61c0cd47e9bd40e41aad
Leonel Abuin <leonelabuin2002@gmail.com>
2024-08-30 16:54:06 -0300

    commit

---
+++ src/brands/brand.entity.ts
@@ -0,0 +1,26 @@
+import { orm } from "../shared/orm.js";
+import { Entity,Property,PrimaryKey,OneToMany, Collection } from "@mikro-orm/core";
+import { BaseEntity } from "../shared/baseEntity.entity.js";
+import { Product } from "../products/product.entity.js";
+
+
+@Entity()
+export class Brand extends BaseEntity{
+    @PrimaryKey()
+    id?: number
+
+    @Property({nullable:false})
+    name!: string
+
+    @Property({nullable:true})
+    description!: string
+
+    @Property({nullable:true})
+    website!: string
+
+    @Property({nullable:true})
+    countryOfOrigin!: string
+
+    @OneToMany(()=>Product,product => product.brand)
+    products = new Collection <Product>(this)
+}
--- src/products/product.entity.ts
+++ src/products/product.entity.ts
@@ -1,8 +1,9 @@
 import { orm } from '../shared/orm.js'
-import { Entity,PrimaryKey,Property,OneToMany, Cascade, Collection, Rel } from '@mikro-orm/core'
+import { Entity,PrimaryKey,Property,OneToMany, Cascade, Collection, Rel, ManyToOne } from '@mikro-orm/core'
 import crypto from 'node:crypto'
 import { Price } from './price.entity.js'
 import { BaseEntity } from '../shared/baseEntity.entity.js'
+import { Brand } from '../brands/brand.entity.js'
 
 
 @Entity()
@@ -31,5 +32,8 @@ export class Product extends BaseEntity {
 
     @OneToMany(()=> Price, price => price.product, {cascade: [Cascade.ALL]})
     prices =  new Collection<Price>(this)
+
+    @ManyToOne(()=> Brand, {nullable:false})
+    brand!:Brand
   
 }
