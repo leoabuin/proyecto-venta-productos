0f9600c5b0a8467d13d37b663efbe490284114bf
Leonel Abuin <leonelabuin2002@gmail.com>
2024-09-12 08:44:50 -0300

    commit

--- src/orders/order.controler.ts
+++ src/orders/order.controler.ts
@@ -12,7 +12,9 @@ function sanitizeOrderInput(req: Request, res: Response, next: NextFunction) {
     total: req.body.total,
     estado: req.body.estado,
     metodo_pago: req.body.metodo_pago,
+    orderItems: req.body.orderItems,
   }
+  
   //more checks here
 
   Object.keys(req.body.sanitizedInput).forEach((key) => {
@@ -24,23 +26,60 @@ function sanitizeOrderInput(req: Request, res: Response, next: NextFunction) {
 }
 
 async function findAll(req: Request, res: Response) {
-    return res.status(401).send({ message: 'metod not omplemented' })
+  try {
+    const orders = await em.find(Order,{},{populate:['orderItems']})
+    res.status(200).json({message:'found all Orders',data:orders})
+  } catch (error: any) {
+    return res.status(500).json({message: error.message})
+  }
 }
 
-function findOne(req: Request, res: Response) {
-  return res.status(401).send({ message: 'metod not omplemented' })
+async function findOne(req: Request, res: Response) {
+  try {
+    const id = Number.parseInt(req.params.id)
+    const order = await em.findOneOrFail(Order,{id},{populate:['orderItems']})
+    res.status(200).json({message:'found order',data:order})
+  } catch (error: any) {
+    return res.status(500).json({message: error.message})
+  }
 }
 
-function add(req: Request, res: Response) {
-  return res.status(401).send({ message: 'metod not omplemented' })
+async function add(req: Request, res: Response) {
+  try {
+    const order = em.create(Order,req.body.sanitizedInput)
+    await em.flush()
+    res.status(201).json({message:'Order created', data:order})
+  } catch (error:any) {
+      res.status(500).json({message: error.message})
+  }
 }
 
 function update(req: Request, res: Response) {
-  return res.status(401).send({ message: 'metod not omplemented' })
+  return res.status(401).send({ message: 'funtion not implemented' })
 }
 
-function remove(req: Request, res: Response) {
-  return res.status(401).send({ message: 'metod not omplemented' })
+async function remove(req: Request, res: Response) {
+  try {
+    const id = Number.parseInt(req.params.id)
+    const order = em.getReference(Order, id)
+    await em.removeAndFlush(order)
+    res.status(200).json({message: 'order deleted'})
+  } catch (error: any) {
+    res.status(500).json({message: error.message})
+  }
 }
+
+
+/*
+  1) cliente puede agregar producto a su carrito con su cantidad (lineas de pedido)
+  2) cuando el cliente decide finalizar su pedido, presiona finalizar pedido
+  nota: en que momento se crea el pedido. 
+  nota: en que momento se crea la linea de pedido
+
+
+*/
+
+
+
 
 export { sanitizeOrderInput, findAll, findOne, add, update, remove }
