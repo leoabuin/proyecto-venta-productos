cc32e4f87943a060d21fc768f80dc62f6cbfa86b
Leonel <leonelabuin2002@gmail.com>
2026-03-09 20:53:41 -0300

    Forzando variables y log de conexion

--- src/shared/orm.ts
+++ src/shared/orm.ts
@@ -1,16 +1,18 @@
 import { MikroORM } from "@mikro-orm/core";
 import { SqlHighlighter } from "@mikro-orm/sql-highlighter";
 
+console.log('Intentando conectar a:', process.env.MYSQLHOST); // <-- Agregá esto para debuguear
+
 export const orm = await MikroORM.init({
     entities: ['dist/**/*.entity.js'],
     entitiesTs: ['src/**/*.entity.ts'],
     type: 'mysql',
-    // Forzamos el uso de las variables de Railway, y si no existen, recién ahí va al localhost
     host: process.env.MYSQLHOST || '127.0.0.1',
     port: Number(process.env.MYSQLPORT) || 3306,
     user: process.env.MYSQLUSER || 'root',
     password: process.env.MYSQLPASSWORD || 'root',
-    dbName: process.env.MYSQLDATABASE || process.env.MYSQL_DATABASE || 'ventaProductos',
+    // IMPORTANTE: Si MYSQLDATABASE está vacío, que NO use ventaProductos, sino railway
+    dbName: process.env.MYSQLDATABASE || process.env.MYSQL_DATABASE || 'railway', 
     debug: true,
     highlighter: new SqlHighlighter(),
     schemaGenerator: {
