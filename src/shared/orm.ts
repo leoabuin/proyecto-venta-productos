4ac65713459410c0fed4dde46350000e7fa67d0f
Leonel <leonelabuin2002@gmail.com>
2026-03-09 19:14:57 -0300

    Fix final: variables de entorno dinámicas

--- src/shared/orm.ts
+++ src/shared/orm.ts
@@ -4,19 +4,14 @@ import { SqlHighlighter } from "@mikro-orm/sql-highlighter";
 export const orm = await MikroORM.init({
     entities: ['dist/**/*.entity.js'],
     entitiesTs: ['src/**/*.entity.ts'],
-    // Intentamos URL completa, si no, intentamos componentes individuales
-    clientUrl: process.env.MYSQL_URL, 
-    driverOptions: process.env.MYSQL_URL ? { 
-        connection: { ssl: { rejectUnauthorized: false } } 
-    } : {},
-    // Si no hay MYSQL_URL, cae en los parámetros manuales o el localhost
-    host: process.env.MYSQLHOST || 'localhost',
+    type: 'mysql',
+    // Forzamos el uso de las variables de Railway, y si no existen, recién ahí va al localhost
+    host: process.env.MYSQLHOST || '127.0.0.1',
     port: Number(process.env.MYSQLPORT) || 3306,
     user: process.env.MYSQLUSER || 'root',
     password: process.env.MYSQLPASSWORD || 'root',
-    dbName: process.env.MYSQLDATABASE || 'ventaProductos',
-    type: 'mysql',
-    debug: true, // Dejalo en true un ratito para ver qué pasa en Railway
+    dbName: process.env.MYSQLDATABASE || process.env.MYSQL_DATABASE || 'ventaProductos',
+    debug: true,
     highlighter: new SqlHighlighter(),
     schemaGenerator: {
       disableForeignKeys: true,
